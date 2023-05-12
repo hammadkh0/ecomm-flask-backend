9d22a543c01aea29c28c3f2c332c2d25621c13c9
hammadkh0 <hammad5718@gmail.com>
2023-05-12 16:25:09 +0500

    Added python runtime.txt file

--- app.py
+++ app.py
@@ -377,6 +377,7 @@ def about():
     return 'About'
 
 
-# if __name__ == '__main__':
-#     # run app in debug mode on port 5000
-#     app.run(debug=True, host='0.0.0.0',port=5000)
+##if __name__ == '__main__':
+##    # run app in debug mode on port 5000
+##    app.run(debug=True, host='0.0.0.0', port=5000)
+##
